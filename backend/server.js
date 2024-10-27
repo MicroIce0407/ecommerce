df6468a3c5253a194c0058ee289ca48fd551674e
Jhuang Hao Yu <wvz3548@gmail.com>
2024-10-28 01:54:14 +0800

    fix DBUrl

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,13 @@ const orderRoutes = require("./routes/orderRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const MONGO_URI = "mongodb://localhost:27017/ecommerce";
-
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("MongoDB connected successfully");
   })
   .catch((err) => {
-    console.error("Error connecting to MongoDB", err);
+    console.error("MongoDB connection error:", err);
   });
 
 app.use(express.json()); // 中間件，用於解析 JSON 請求
